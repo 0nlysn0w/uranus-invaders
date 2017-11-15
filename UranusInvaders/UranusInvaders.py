e029480de109177f322350e5b903c8abee27ec0d
Ramon Meijers <ramon.meijers@hotmail.com>
2017-11-15 15:06:50 +0100

    hover over changed the text

--- UranusInvaders/UranusInvaders.py
+++ UranusInvaders/UranusInvaders.py
@@ -17,7 +17,36 @@ pygame.display.set_icon(pygame.image.load("Assets/ball.png"))
 pygame.display.set_caption("Uranus invaders")
 
 
-screen.fill(white)
+
+class MenuItem(pygame.font.Font):
+    def __init__(self, text, font=None, font_size=30,
+                 font_color=(255, 255, 255), pos_x = 0, pos_y = 0):
+        pygame.font.Font.__init__(self, font, font_size)
+        self.text = text
+        self.font_size = font_size
+        self.font_color = font_color
+        self.label = self.render(self.text, 1, self.font_color)
+        self.width = self.label.get_rect().width
+        self.height = self.label.get_rect().height
+        self.dimensions = (self.width, self.height)
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.position = pos_x, pos_y
+ 
+    def set_position(self, x, y):
+        self.position = (x, y)
+        self.pos_x = x
+        self.pos_y = y
+ 
+    def set_font_color(self, rgb_tuple):
+        self.font_color = rgb_tuple
+        self.label = self.render(self.text, 1, self.font_color)
+ 
+    def is_mouse_selection(self, posx, posy):
+        if (posx >= self.pos_x and posx <= self.pos_x + self.width) and \
+            (posy >= self.pos_y and posy <= self.pos_y + self.height):
+                return True
+        return False
 
 class GameMenu() :
     def __init__(self, screen, items):
@@ -29,33 +58,30 @@ class GameMenu() :
         self.font = myfont
 
         for index, item in enumerate(items):
-            label = self.font.render(item, 1, white)
+            menu_item = MenuItem(item)
  
-            width = label.get_rect().width
-            height = label.get_rect().height
+            t_h = len(items) * menu_item.height
+            pos_x = (self.scr_width / 2) - (menu_item.width / 2)
+            pos_y = (self.scr_height / 2) - (t_h / 2) + ((index * 2) + index * menu_item.height)
  
-            posx = (self.scr_width / 2) - (width / 2)
-            # t_h: total height of text block
-            t_h = len(items) * height
-            posy = (self.scr_height / 2) - (t_h / 2) + (index * height)
-            #sets label properties in an array
-            self.items.append([item, [label, (width, height), (posx, posy)]])
+            menu_item.set_position(pos_x, pos_y)
+            self.items.append(menu_item)
 
     def run(self) :
         running = True
         while running:
-            for name, labelProperties in self.items:
-                #reads label properties from  the array set in the __INIT__
-                label = labelProperties[0]
-                posx = labelProperties[2][0]
-                posy = labelProperties[2][1]
-                self.screen.blit(label, (posx, posy))
+            self.screen.fill((0, 0, 0))
+            for item in self.items:
+                mouseProperties = pygame.mouse.get_pos()
+                if item.is_mouse_selection(mouseProperties[0], mouseProperties[1]):
+                    item.set_font_color((255, 0, 0))
+                    item.set_italic(True)
+                else:
+                    item.set_font_color((255, 255, 255))
+                    item.set_italic(False)
+                self.screen.blit(item.label, item.position)
 
             for i in pygame.event.get():
-                print(i);
-                print("Type:"  + str(i.type))
-                if hasattr(i, "key"):
-                    print("key:"  + str(getattr(i, "key")))
 
                 if i.type == pygame.QUIT or (hasattr(i, "key") and getattr(i, "key") == 27):
                     running = False
@@ -65,10 +91,15 @@ class GameMenu() :
                     pygame.display.update()
                     self.clock.tick(30)
 
+    def isMouseSelection(self, posx, posy):
+        if (posx >= self[2][0] and posx <= self[2][0] + self[1][0]) and (posy >= self[2][1] and posy <= self[2][1] + self[1][1]):
+                return True
+        return False
+
 if __name__ == "__main__":
     # Creating the screen
     screen = pygame.display.set_mode(size)
-    menuItems = ('Start', 'Quit')
+    menuItems = ("Space Invaders", "Asteroids", "Quit")
     pygame.display.set_caption('Game Menu')
     gm = GameMenu(screen, menuItems)
     gm.run()
