73973efdeb5ef66eb0d8562b2176f7003fb78aaa
Ramon Meijers <ramon.meijers@hotmail.com>
2017-11-21 10:58:19 +0100

    The program can exit now

--- UranusInvaders/UranusInvaders.py
+++ UranusInvaders/UranusInvaders.py
@@ -19,7 +19,7 @@ pygame.display.set_caption("Uranus invaders")
 
 
 class MenuItem(pygame.font.Font):
-    def __init__(self, text, font=None, font_size=30,
+    def __init__(self, text, redir, font=None, font_size=30,
                  font_color=(255, 255, 255), pos_x = 0, pos_y = 0):
         pygame.font.Font.__init__(self, font, font_size)
         self.text = text
@@ -32,6 +32,7 @@ class MenuItem(pygame.font.Font):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.position = pos_x, pos_y
+        self.redir = redir
  
     def set_position(self, x, y):
         self.position = (x, y)
@@ -49,7 +50,7 @@ class MenuItem(pygame.font.Font):
         return False
 
 class GameMenu() :
-    def __init__(self, screen, items):
+    def __init__(self, screen, items, redir):
         self.screen = screen
         self.scr_width = self.screen.get_rect().width
         self.scr_height = self.screen.get_rect().height
@@ -58,12 +59,12 @@ class GameMenu() :
         self.font = myfont
 
         for index, item in enumerate(items):
-            menu_item = MenuItem(item)
+            menu_item = MenuItem(item, redir[index])
  
             t_h = len(items) * menu_item.height
             pos_x = (self.scr_width / 2) - (menu_item.width / 2)
             pos_y = (self.scr_height / 2) - (t_h / 2) + ((index * 2) + index * menu_item.height)
- 
+
             menu_item.set_position(pos_x, pos_y)
             self.items.append(menu_item)
 
@@ -76,6 +77,12 @@ class GameMenu() :
                 if item.is_mouse_selection(mouseProperties[0], mouseProperties[1]):
                     item.set_font_color((255, 0, 0))
                     item.set_italic(True)
+                    if pygame.mouse.get_pressed()[0]:
+                        print("left clicked")
+                        print(item.redir)
+                        if item.redir == "quit":
+                            pygame.quit()
+                            quit()
                 else:
                     item.set_font_color((255, 255, 255))
                     item.set_italic(False)
@@ -100,6 +107,7 @@ if __name__ == "__main__":
     # Creating the screen
     screen = pygame.display.set_mode(size)
     menuItems = ("Space Invaders", "Asteroids", "Quit")
+    menuRedirect = ("tim", "ramon", "quit")
     pygame.display.set_caption('Game Menu')
-    gm = GameMenu(screen, menuItems)
+    gm = GameMenu(screen, menuItems, menuRedirect)
     gm.run()
