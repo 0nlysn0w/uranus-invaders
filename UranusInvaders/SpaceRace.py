26514de2a91a1484e4c38a9a3a5236fbe7152b23
Joost Stam <joost.stam@gmail.com>
2017-12-18 18:12:59 +0100

    Redirect/Render to SpaceRaceGame() to play the game

--- UranusInvaders/SpaceRace.py
+++ UranusInvaders/SpaceRace.py
@@ -5,15 +5,6 @@ from BaseRenderer import BaseRenderer
 class SpaceRaceGame():
     def __init__(self, pyg, screen):
         self.pyg = pyg
-
-    def run(self, event):
-        return
-
-class SpaceRace():
-    def __init__(self, pyg, screen):
-        #Hier kan je plaatjes inladen en in self.{NAAM} zetten en in de run functie dan weer gebruiken, dit zorgt ervoor dat je tijdens het spelen niets hoeft in te laden
-        print("init SpaceRace")
-        self.pyg = pyg
         self.myfont = self.pyg.font.SysFont("monospace", 30)
         self.screen = screen
         self.spaceship = pyg.image.load("Assets/spaceship-basic.png")
@@ -26,6 +17,64 @@ class SpaceRace():
         self.spaceshipY = (self.height - self.spaceshipHeight)/2
         self.rotation = 0
         self.speed = 10
+
+    def background(self):
+
+        rotatedimg = self.pyg.transform.rotate(self.spaceship, self.rotation)
+
+        self.screen.blit(self.track, (self.spaceshipX, self.spaceshipY))
+        self.screen.blit(rotatedimg, ((self.width / 2) - (self.spaceshipWidth/2), (self.height / 2) - (self.spaceshipHeight/2)))
+
+    def run(self, event):
+        keys = self.pyg.key.get_pressed()
+
+        #print(keys)
+
+        # North
+        if keys[self.pyg.K_UP] or keys[self.pyg.K_w]:
+           self.spaceshipY += self.speed
+           self.rotation = 0
+
+        # East
+        if keys[self.pyg.K_RIGHT] or keys[self.pyg.K_d]:
+            self.spaceshipX -= self.speed
+            self.rotation = 270
+
+        # South
+        if keys[self.pyg.K_DOWN] or keys[self.pyg.K_s]:
+           self.spaceshipY -= self.speed
+           self.rotation = 180
+
+        # West
+        if keys[self.pyg.K_LEFT] or keys[self.pyg.K_a]:
+            self.spaceshipX += self.speed
+            self.rotation = 90
+
+        # North East
+        if (keys[self.pyg.K_UP] or keys[self.pyg.K_w]) and (keys[self.pyg.K_RIGHT] or keys[self.pyg.K_d]):
+            self.rotation = 315
+
+        # South East
+        if (keys[self.pyg.K_RIGHT] or keys[self.pyg.K_d]) and (keys[self.pyg.K_DOWN] or keys[self.pyg.K_s]):
+           self.rotation = 225
+
+        # South West
+        if (keys[self.pyg.K_DOWN] or keys[self.pyg.K_s]) and (keys[self.pyg.K_LEFT] or keys[self.pyg.K_a]):
+           self.rotation = 135  
+
+        # North West
+        if (keys[self.pyg.K_LEFT] or keys[self.pyg.K_a]) and (keys[self.pyg.K_UP] or keys[self.pyg.K_w]):
+           self.rotation = 45
+
+class SpaceRace():
+    def __init__(self, pyg, screen):
+        #Hier kan je plaatjes inladen en in self.{NAAM} zetten en in de run functie dan weer gebruiken, dit zorgt ervoor dat je tijdens het spelen niets hoeft in te laden
+        print("init SpaceRace")
+        self.pyg = pyg
+        self.myfont = self.pyg.font.SysFont("monospace", 30)
+        self.screen = screen
+        self.width = pyg.display.Info().current_w
+        self.height = pyg.display.Info().current_h
         self.state = "menu"
         self.menu_item = MenuItem("Continue", "game", None, 80)
         self.menu_item.set_position((self.width/2)-(self.menu_item.width/2),(self.height/2))
